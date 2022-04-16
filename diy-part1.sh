90a4bb1b01167ced11b0e04d82b4b339eea395f2
yazhoumimimao <73220318+yazhoumimimao@users.noreply.github.com>
2022-04-17 07:01:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
  #Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 
@@ -10,9 +10,9 @@
  #Description: OpenWrt DIY script part 1 (Before Update feeds)
 
 
- Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+ #Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
- Add a feed source
- echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
- echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.
+ #Add a feed source
+ #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+ #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.
